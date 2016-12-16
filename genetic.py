9dc973636b43b165ae0be3ce215d58fb799d2546
wegier <artur.hd789@gmail.com>
2016-12-16 15:25:40 +0100

    Szkielet algorytmu genetycznego oraz do skalowania przystosowania

--- genetic.py
+++ genetic.py
@@ -9,45 +9,85 @@ addaptation = [];
 pop_size = 0;
 p = 0;
 k = 0;
+board = [];
+collectpts = [];
+providepts = [];
+T = 0;
 
 
-#Losuje populacje liczaca pops osobnikow
-#pl- liczba platform, kl - liczba surowcow
-def rand_population(pops, pl, kl):
+#pops - rozmiar populacji
+#num_it - liczba iteracji
+#r - patrz funkcja addapt_scaling()
+#sel_size, elit - patrz funkcja selection()
+#x, mut - patrz funkcja new_population()
+#pozostale zmienne - oznaczenia takie same jak stosowane w innych miejscach
+#z dodana litera l
+def run_gen_algorithm(pl, kl, boardl, collectptsl, provideptsl, Tl, pops,
+	num_it, r, sel_size, elit, x, mut):
 	global p;
 	global k;
 	global pop_size;
+	global board;
+	global collectpts;
+	global providepts;
+	global T;
+	
+	p = pl;
+	k = kl;
+	board = [];
+	collectpts = [];
+	providepts = [];
+	T = 0;
+	board = [];
+
+    pop_size = pops;
+	rand_population();
+	
+	for(i in range(num_it)):
+		addapt_scaling(r);
+		selection(sel_size, elit);
+		new_population(x, mut):
+
+#Losuje populacje liczaca 
+def rand_population():
 	global chroms;
 	global addaptation;
 	chroms = [];
 	addaptation = [];
-	pop_size = pops;
-	p = pl;
-	k = kl;
+	
 	for i in range(pop_size):
 		chroms.append(randChrom(p, k));
-	pass;
 
 
 #Po wykonaniu ponizszej funkcji w liscie addaptation zapisane jest przystosowanie
 #osobnika odpowiadajacych danemu indeksowi w liscie 
 
 #zastosowano skalowanie liniowe
-#parametr k to maksymalna wartosc przeskalowanego przystosowania 
+#parametr r to maksymalna wartosc przeskalowanego przystosowania 
 #przy zalozeniu, ze srednie przystosowanie jest rowne 1
 
-def addapt_scaling(k):
+def addapt_scaling(r):
 	#policz nieprzeskalowane przystosowanie kazdego z osobnikow
-	pass;
-
+	addaptation = [];
+	for(chr in chroms):
+		addaptation.append(find_paths(p, k, board, collectpts, providepts, chr, T));
+	#Srednia przystosowania
+	av = float(sum(addaptation)) / float(len(addaptation));
+	#najlepsze przystosowanie
+	best = float(max(addaptation));
+	a = (r - 1)/(best - av);
+	b = (best - (r*av))/(best - av);
+	#dokonaj przeskalowania dla kazdego elementu adaptation
+	addaptation[:] = [(a*x + b) for x in addaptation]; 
+	
 #selekcja osobnikow - chroms - populacja zapisana za pomoca chromosomow
 #(wybrane osobniki zostana wykorzystane do krzyzowania i mutacji)
-# n - liczba osobnikow powstajaca w wyniku
-#k - liczba osobnikow o najlepszej ocenie przystosowania
+#sel_size - liczba osobnikow powstajaca w wyniku selekcji
+#elit - liczba osobnikow o najlepszej ocenie przystosowania
 #ktora na pewno przezyje podczas selekcji (k<=n, gdy
 #nie jest stosowana strategia elitarna k = 0)
 #w tym projekcie zastosowano selekcje turniejowa.
-def selection(n, k):
+def selection(sel_size, elit):
 	global chroms;
 	pass;
 
@@ -55,8 +95,8 @@ def selection(n, k):
 #osobnikow powstalych w wyniku krzyzowania i osobnikow powstalych 
 #w wyniku mutacji
 #x - liczba osobnikow powstalych z krzyzowania
-#m - liczba osobnikow powstalych w wyniku mutacji
-def new_population():
+#mut - liczba osobnikow powstalych w wyniku mutacji
+def new_population(x, mut):
 	pass;
 
 	
